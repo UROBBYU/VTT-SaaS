f283972062358b9b923e802a42f4a9a2b9fa2c52
UROBBYU <srobby228@gmail.com>
2024-12-21 10:59:10 +0200

    Added retries for failed DB requests

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -7,12 +7,25 @@ export default async () => {
 
 	if (!user) throw new Error('Not Authorized.')
 
-	const userDB = await prisma.user.findUnique({
-		where: { clerkId: user.id },
-		select: { uploads: true }
-	})
+	const checkDB = async (updateTime: number): Promise<typeof userDB> => {
+		let userDB = await prisma.user.findUnique({
+			where: { clerkId: user.id },
+			select: { uploads: true }
+		})
 
-	if (!userDB) throw new Error('No DB entry found.')
+		const sinceUpdate = Date.now() - updateTime
+		if (!userDB && (sinceUpdate < 6e4)) {
+			await new Promise(res => setTimeout(res, 5e3))
+
+			userDB = await checkDB(updateTime)
+		}
+
+		return userDB
+	}
+
+	const userDB = await checkDB(user.updatedAt)
+
+	if (!userDB) throw new Error(`No DB entry found for (${user.id}).`)
 
 	return <div>{userDB.uploads.map(upload => <SidebarItem
 		key={upload.id}
