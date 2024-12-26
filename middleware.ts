7912c2b96de738fec54444cd27c4e4cf695a8595
UROBBYU <srobby228@gmail.com>
2024-12-26 08:06:17 +0200

    Added new user redirection to login page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware(async auth => void await auth.protect())
 
 export const config = {
 	matcher: [
